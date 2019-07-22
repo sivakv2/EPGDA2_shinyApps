6e2ae9e6d07b726e42d7e056ae8bfe7189cdd88e
siva <sivakumarvi@gmail.com>
2019-07-22 12:29:58 +0530

    for EPGDA class

---
+++ shiny_app_01.R
@@ -0,0 +1,11 @@
+# Load the shiny package
+library(shiny)
+
+# Define UI for the application
+ui <- fluidPage()
+
+# Define the server logic
+server <- function(input, output) {}
+
+# Run the application
+shinyApp(ui = ui, server = server)
