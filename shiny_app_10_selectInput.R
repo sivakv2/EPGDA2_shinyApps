25c316af7bb812959d92c5819f635082dbc6bf54
siva <sivakumarvi@gmail.com>
2019-08-15 20:31:44 +0530

    add selectInput

---
+++ shiny_app_10_selectInput.R
@@ -0,0 +1,32 @@
+library(shiny)
+library(ggplot2)
+library(gapminder)
+library(DT)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Gapminder Data"),
+   
+   sidebarLayout(
+     sidebarPanel(
+              selectInput("year","Year",c("1957","1962","1967","1972","1978"))
+                  ),
+   
+   mainPanel (
+            dataTableOutput("table")
+            )
+      )
+   )
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  #Create the table
+  output$table <- renderDataTable(gapminder)
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
