ffb28eb69b96ad3556c98b2c8263347e2a7861c1
siva <sivakumarvi@gmail.com>
2019-08-15 20:07:12 +0530

    renderTable

---
+++ shiny_app_07a.R
@@ -0,0 +1,21 @@
+library(shiny)
+library(gapminder)
+
+# Define UI for application for table data
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Gapminder Data"),
+      tableOutput("table")
+)
+
+# Define server logic required to fetch table data
+server <- function(input, output) {
+  
+  #Create the table
+  output$table <- renderTable(gapminder)
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
