06a0f3f0548ffaf01093bad3fade3221623565dd
siva <sivakumarvi@gmail.com>
2019-08-15 19:53:28 +0530

    sidebarLayout

---
+++ shiny_app_09.R
@@ -0,0 +1,30 @@
+library(shiny)
+library(ggplot2)
+library(gapminder)
+library(DT)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Gapminder Data"),
+   
+   sidebarLayout(
+     sidebarPanel("Text"),
+   
+   mainPanel (
+            dataTableOutput("table")
+            )
+      )
+   )
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  #Create the table
+  output$table <- renderDataTable(gapminder)
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
